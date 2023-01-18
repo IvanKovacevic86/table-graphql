18362d49bf55d927f6f3847c5e67de741d08a39f
Nemanja Miljkovic <nmiljkovic89@gmail.com>
2023-01-18 03:10:46 +0100

    added table

--- src/App.js
+++ src/App.js
@@ -1,5 +1,40 @@
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+} from "@mui/material";
+
+const columns = [
+  { label: "NAME", id: "name" },
+  { label: "USERNAME", id: "username" },
+  { label: "EMAIL", id: "email" },
+  { label: "", id: "actions" },
+];
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Table>
+        <TableHead>
+          <TableRow sx={{ backgroundColor: "#D3D3D3" }}>
+            {columns.map((column) => (
+              <TableCell key={column.id}>{column.label}</TableCell>
+            ))}
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          <TableRow>
+            <TableCell>fdgg</TableCell>
+            <TableCell>dfdf</TableCell>
+            <TableCell>fdfdf</TableCell>
+            <TableCell>ddfd</TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+    </div>
+  );
 }
 
 export default App;
