36e66277e1bac7d9624caad5e8cc792113245412
Nemanja Miljkovic <nmiljkovic89@gmail.com>
2023-01-19 02:24:46 +0100

    query data

--- src/App.js
+++ src/App.js
@@ -5,6 +5,20 @@ import {
   TableHead,
   TableRow,
 } from "@mui/material";
+import { useQuery, gql } from "@apollo/client";
+
+const GET_USERS = gql`
+  query {
+    users {
+      data {
+        id
+        name
+        username
+        email
+      }
+    }
+  }
+`;
 
 const columns = [
   { label: "NAME", id: "name" },
@@ -14,6 +28,10 @@ const columns = [
 ];
 
 function App() {
+  const { data, loading } = useQuery(GET_USERS);
+
+  if (loading) return <div>Loading...</div>;
+
   return (
     <div className="App">
       <Table>
@@ -25,13 +43,13 @@ function App() {
           </TableRow>
         </TableHead>
         <TableBody>
-          <TableRow>
-            <TableCell>fdgg</TableCell>
-            <TableCell>dfdf</TableCell>
-            <TableCell>fdfdf</TableCell>
-            <TableCell>ddfd</TableCell>
-            <TableCell>ddfd</TableCell>
-          </TableRow>
+          {data.users.data.map((user) => (
+            <TableRow key={user.id}>
+              <TableCell>{user.name}</TableCell>
+              <TableCell>{user.username}</TableCell>
+              <TableCell>{user.email}</TableCell>
+            </TableRow>
+          ))}
         </TableBody>
       </Table>
     </div>
